d7de60f6ea72a64568ec93b52b180cd514e21e0e
Sliim <sliim@mailoo.org>
2017-10-23 17:36:12 +0200

    Add attribute for custom decoders
    Custom decoders will be deployed in etc/decoders/local_decoder.xml

--- attributes/default.rb
+++ attributes/default.rb
@@ -60,3 +60,7 @@ default['ossec']['conf']['agent']['client']['server-ip'] = node['ossec']['agent_
 # way. We leave this blank by default because Chef is better at
 # distributing agent configuration than OSSEC is.
 default['ossec']['agent_conf'] = []
+
+# Local decoders to deploy in etc/decoders/local_decoder.xml config file.
+# Only for server configuration, will be ignored if empty.
+default['ossec']['local_decoders'] = {}
